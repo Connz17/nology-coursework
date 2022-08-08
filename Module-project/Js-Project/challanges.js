2db3d6b288347c9deea4bac75751a9dc59dec7a5
Connz17 <connel_n@hotmail.com>
2022-08-08 08:23:20 +0100

    JS challanges file created and started

---
+++ Module-project/Js-Project/challanges.js
@@ -0,0 +1,10 @@
+// 1.Negative and positive number
+const isNumberPositive(number) => {
+    if (number < 0) {
+        console.log("False number is negative")
+    } else {
+        console.log("True number is positive")
+    }
+
+    isNumberPositive(5)
+}
