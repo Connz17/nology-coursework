c20f39b23b440414ae80fe334dca967f9ec40675
Connz17 <connel_n@hotmail.com>
2022-08-08 08:50:48 +0100

    Loop array added to chllenge 4

--- Module-project/Js-Project/challanges.js
+++ Module-project/Js-Project/challanges.js
@@ -1,8 +1,11 @@
 // 1.Negative and positive number
 
+(number = -5)
 const isNumberPositive = (number) => {
-    return "false" 
+    return "false"
 
+
+    isNumberPositive();
 
 }
 
@@ -11,6 +14,11 @@ const isNumberPositive = (number) => {
 // 4. Return last name in array
 
 let names = (["charlie", "Rob", "Andy"]);
+console.log(names[2]);
 
-console.log(names[2])
+let players = ["Steve", "Peter", "Sam", "John"];
+    for (let index = 3; index < players.length; index++) {
+        console.log(players[index]);
+    }
+
 
